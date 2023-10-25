4e2b5462c34316d25e2f576a3c387a622eb67ecc
raufschlaeger <robert.aufschlaeger@th-deg.de>
2023-10-25 13:00:22 +0200

    Add test_suppress.py

---
+++ test/test_suppress.py
@@ -0,0 +1,73 @@
+import sys
+import unittest
+import pandas as pd
+from anonymize.suppress import suppress_categorical, suppress_float, remove_groups
+from anonymetrics.anonymetrics import get_groups
+
+sys.path.append(".")
+
+
+class TestGeneralizations(unittest.TestCase):
+    def test_suppress_float(self):
+        # Define user data
+        users = {
+            'user_id': [1, 2, 3, 4, 5],
+            'age': [23, 23, 40, 29, 45],
+            'income': [40000, 40000, 60000, 45000, 90000]
+        }
+
+        # Create a DataFrame
+        df = pd.DataFrame(users)
+
+        # Add a attribute column with integer values
+        df['credit_score'] = [720, 680, 590, 650, 780]
+
+        suppress_float(df, 3)
+        self.assertEqual(df["credit_score"][0], 684.0)
+        self.assertEqual(df["credit_score"][3], 684.0)
+
+    def test_suppress_categorical(self):
+        # Set up a dataframe for testing
+        self.df = pd.DataFrame({
+            'A': ['cat', 'dog', 'dog', 'bird', 'bird'],
+            'B': ['apple', 'orange', 'banana', 'apple', 'banana'],
+            'C': ['red', 'blue', 'red', 'green', 'blue']
+        })
+
+        # Call the suppress_categorical function
+        suppress_categorical(self.df, 'dog', 0)
+
+        # Expected output
+        expected_output = pd.DataFrame({
+            'A': ['cat', frozenset({'bird', 'dog', 'cat'}), frozenset({'bird', 'dog', 'cat'}), 'bird', 'bird'],
+            'B': ['apple', 'orange', 'banana', 'apple', 'banana'],
+            'C': ['red', 'blue', 'red', 'green', 'blue']
+        })
+
+        # Assert that the output matches the expected output
+        pd.testing.assert_frame_equal(self.df, expected_output)
+
+    def test_remove_groups(self):
+        # Create a DataFrame for testing
+        df = pd.DataFrame({
+            'A': ['apple', 'banana', 'cherry', 'banana', 'apple', 'apple'],
+            'B': ['red', 'yellow', 'red', 'red', 'yellow', 'red'],
+            'C': ['small', 'large', 'medium', 'large', 'small', 'medium']
+        })
+
+        # Test if groups with less than 2 records are removed correctly
+
+        groups = get_groups(df, [0, 1])
+        remove_groups(df, [0, 1], 2)
+
+        expected_result = pd.DataFrame({
+            'A': ['apple', 'apple'],
+            'B': ['red', 'red'],
+            'C': ['small', 'medium']
+        })
+        # print(df)
+        pd.testing.assert_frame_equal(df.reset_index(drop=True), expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
